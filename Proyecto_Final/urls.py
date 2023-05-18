51a423309d4d77910c57efb1cb0c733a2631197b
Misael Solano <josue.solano@fygdevelopers.com>
2023-05-18 14:58:15 -0600

    comienzo del proyecto

---
+++ Proyecto_Final/urls.py
@@ -0,0 +1,22 @@
+"""
+URL configuration for Proyecto_Final project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/"""
+
+from django.contrib.auth import login
+from django.contrib.auth.views import logout_then_login
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('cuenta.urls')),
+
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ cuenta/urls.py
@@ -0,0 +1,17 @@
+#from django.contrib.auth import login, logout
+from django.contrib.auth.views import logout_then_login,LoginView
+from django.contrib import admin
+from django.urls import path, include
+from cuenta import views
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home', views.home, name='home'),
+    #path('login/', views.login, name='login'),
+    path('register/', views.register, name= 'register'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.exit, name = 'exit' ),
+]
