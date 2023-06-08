a6b41e6d0fd2e49dc0a51278e84955277efe75e7
oscargop <oscargop@gmail.com>
2023-06-07 20:07:48 -0600

    Nuevos Templates - Oscar

--- Proyecto_Final/settings.py
+++ Proyecto_Final/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Proyecto_Final.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -90,9 +90,9 @@ DATABASES = {
         'PORT': '7663',
     }
 }
-"""
 
-DATABASES = {
+
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ecoffee',
@@ -102,6 +102,7 @@ DATABASES = {
         'PORT': '5432',
     }
  }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
